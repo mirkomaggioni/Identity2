0e60b022cd5c3d6958ab2e5a9c655d5b8390a6d4
Mirko Maggioni <mirko.maggioni@sintel.net>
2017-12-30 12:20:29 +0100

    models are added

---
+++ Identity2/Models/IdentityModels.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Identity2.Models
+{
+	public class ApplicationUser : IdentityUser
+	{
+		public string Username { get; set; }
+
+		public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
+		{
+			var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+			return userIdentity;
+		}
+	}
+
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	{
+		public ApplicationDbContext() : base("DefaultConnection", throwIfV1Schema: false) {}
+		public static ApplicationDbContext Create()
+		{
+			return new ApplicationDbContext();
+		}
+	}
+}
