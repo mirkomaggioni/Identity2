47d307e99766d2dc0125c8297410ee852d777460
Mirko Maggioni <mirko.maggioni@sintel.net>
2017-12-30 12:59:58 +0100

    account controller is added

---
+++ Identity2/Controllers/Api/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Identity2.App_Start;
+using System.Web.Http;
+
+namespace Identity2.Controllers.Api
+{
+	[Authorize]
+	public class AccountController : ApiController
+	{
+		private readonly ApplicationUserManager _applicationUserManager;
+
+		public AccountController(ApplicationUserManager applicationUserManager)
+		{
+			_applicationUserManager = applicationUserManager;
+		}
+    }
+}
