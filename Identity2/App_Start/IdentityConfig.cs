9ddd8f3075d9a4819f6e4e988e96c3cbea5b813f
Mirko Maggioni <mirko.maggioni@sintel.net>
2017-12-30 12:54:23 +0100

    identity config is added

---
+++ Identity2/App_Start/IdentityConfig.cs
@@ -0,0 +1,46 @@
+﻿using Identity2.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Identity2.App_Start
+{
+	public class ApplicationUserManager : UserManager<ApplicationUser>
+	{
+		public ApplicationUserManager(IUserStore<ApplicationUser> store) : base(store)
+		{
+		}
+
+		public static ApplicationUserManager Create(IdentityFactoryOptions<ApplicationUserManager> options, IOwinContext context)
+		{
+			var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context.Get<ApplicationDbContext>()));
+			manager.UserValidator = new UserValidator<ApplicationUser>(manager)
+			{
+				AllowOnlyAlphanumericUserNames = false,
+				RequireUniqueEmail = true
+			};
+
+			manager.PasswordValidator = new PasswordValidator
+			{
+				RequiredLength = 8,
+				RequireNonLetterOrDigit = true,
+				RequireDigit = true,
+				RequireLowercase = true,
+				RequireUppercase = true
+			};
+
+			var dataProtectionProvider = options.DataProtectionProvider;
+			if (dataProtectionProvider != null)
+			{
+				manager.UserTokenProvider = new DataProtectorTokenProvider<ApplicationUser>(dataProtectionProvider.Create("ASP.NET Identity"));
+			}
+
+			return manager;
+		}
+	}
+}
