c83d6dc2b17568f441790ddd767478bc98547781
Mirko Maggioni <mirko.maggioni@sintel.net>
2017-12-30 14:01:34 +0100

    GetUserInfoAsync is added

--- Identity2/Controllers/Api/AccountController.cs
+++ Identity2/Controllers/Api/AccountController.cs
@@ -1,4 +1,11 @@
 ﻿using Identity2.App_Start;
+using Identity2.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Identity2.Controllers.Api
@@ -7,10 +14,55 @@ namespace Identity2.Controllers.Api
 	public class AccountController : ApiController
 	{
 		private readonly ApplicationUserManager _applicationUserManager;
+		private ApplicationUserManager _userManager;
 
 		public AccountController(ApplicationUserManager applicationUserManager)
 		{
 			_applicationUserManager = applicationUserManager;
 		}
-    }
+
+		[Route("Logout")]
+		public IHttpActionResult Logout()
+		{
+			Authentication.SignOut(CookieAuthenticationDefaults.AuthenticationType);
+			return Ok();
+		}
+
+		[Route("UserInfo")]
+		public async Task<IHttpActionResult> GetUserInfoAsync()
+		{
+			var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+
+			if (user == null)
+			{
+				return null;
+			}
+
+			return Ok(new UserInfoModel
+			{
+				Username = user.Username,
+				Email = user.Email
+			});
+		}
+
+		private IAuthenticationManager Authentication
+		{
+			get
+			{
+				return Request.GetOwinContext().Authentication;
+			}
+		}
+
+		private ApplicationUserManager UserManager
+		{
+			get
+			{
+				return _userManager ?? Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+			}
+			set
+			{
+				_userManager = value;
+			}
+		}
+	}
 }
