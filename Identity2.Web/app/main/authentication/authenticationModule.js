da91cb4e5d4544941154fc393be4ffd09e083d3b
Mirko Maggioni <mirko.maggioni@gmail.com>
2018-01-02 15:19:54 +0100

    authentication module is added

---
+++ Identity2.Web/app/main/authentication/authenticationModule.js
@@ -0,0 +1,14 @@
+﻿(function (window, angular) {
+  'use-strict';
+  angular.module('authenticationModule', ['ui.router'])
+    .config(function ($stateProvider) {
+      $stateProvider.state('home.login', {
+        url: '/home/login',
+        templateUrl: 'app/main/authentication/login.html',
+        controller: 'loginCtrl'
+      });
+    })
+    .controller('loginCtrl', function () {
+
+    });
+})(window, window.angular)
