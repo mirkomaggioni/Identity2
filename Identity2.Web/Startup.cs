0f4c7e10b03c9396529665d541fe615dbfc49b32
Mirko Maggioni <mirko.maggioni@gmail.com>
2018-01-02 17:16:15 +0100

    autofac is added

--- Identity2.Web/Startup.cs
+++ Identity2.Web/Startup.cs
@@ -3,6 +3,10 @@ using Microsoft.Owin.Security.Cookies;
 using Owin;
 using Identity2.Web.App_Start;
 using Identity2.Web.Models;
+using Autofac;
+using System.Reflection;
+using Autofac.Integration.WebApi;
+using System.Web.Http;
 
 [assembly: OwinStartup(typeof(Identity2.Web.Startup))]
 
@@ -15,6 +19,27 @@ namespace Identity2.Web
 			app.CreatePerOwinContext(ApplicationDbContext.Create);
 			app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
 			app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+			var containerBuilder = new ContainerBuilder();
+			containerBuilder.RegisterType<ApplicationUserManager>();
+
+			containerBuilder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+			var container = containerBuilder.Build();
+
+			var config = new HttpConfiguration
+			{
+				DependencyResolver = new AutofacWebApiDependencyResolver(container)
+			};
+
+			config.MapHttpAttributeRoutes();
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+
+			app.UseWebApi(config);
 		}
 	}
 }
