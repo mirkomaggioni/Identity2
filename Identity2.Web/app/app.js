ed77f06d6cb03bf9845ede0989041c302e2dd2bf
Mirko Maggioni <mirko.maggioni@gmail.com>
2018-01-02 15:02:46 +0100

    main pages and javascript files are added

---
+++ Identity2.Web/app/app.js
@@ -0,0 +1,11 @@
+﻿(function (window, angular) {
+  'use-strict';
+  angular.module('app', ['ui.router', 'mainModule'])
+    .config(function ($urlRouterProvider, $locationProvider) {
+      $urlRouterProvider.otherwise('/main/login');
+      $locationProvider.hashPrefix('');
+    })
+    .run(function ($state) {
+      $state.go('main.login');
+    });
+})(window, window.angular);
---
+++ Identity2.Web/app/main/main.js
@@ -0,0 +1,16 @@
+﻿(function (window, angular) {
+  'use-strict';
+  angular.module('mainModule', ['ui.router', 'authenticationModule'])
+    .config(function ($stateProvider) {
+      var mainState = {
+        name: 'main',
+        url: '/main',
+        views: {
+          'header': { templateUrl: 'app/main/header.html' },
+          'main': { templateUrl: 'app/main/main.html' }
+        }
+      }
+
+      $stateProvider.state(mainState);
+    })
+})(window, window.angular)
